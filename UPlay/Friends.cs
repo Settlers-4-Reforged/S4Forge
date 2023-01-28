0814763c74256d165ab39a2f6a9edf635483a509
WizzardMaker <wizzardmaker@gmail.com>
2023-01-28 02:32:36 +0100

    fixed invite API

--- UPlay/Friends.cs
+++ UPlay/Friends.cs
@@ -48,8 +48,9 @@ namespace S4_UIEngine.UPlay {
             public Int32 unknownProbablySize; //seems to be "15"
         };
 
-        private unsafe delegate bool _UPlayFriendsGetFriendList(uint size/*0xFFu*/, UPlayFriendList* list);
-        private unsafe delegate bool _UPlayFriendsInviteToGame(string accountId, InviteParameters* param);
+        // Filter -> 1 = "request_sent"; 2 = "request_received"; 4 = "friends"; 8 = "blacklisted"; 0xFF = "all
+        private unsafe delegate bool _UPlayFriendsGetFriendList(uint filter, UPlayFriendList* list);
+        private unsafe delegate bool _UPlayFriendsInviteToGame(IntPtr accountId, InviteParameters* param);
 
         private static _UPlayFriendsGetFriendList UPlayFriendsGetFriendList;
         private static _UPlayFriendsInviteToGame UPlayFriendsInviteToGame;
@@ -66,7 +67,7 @@ namespace S4_UIEngine.UPlay {
             unsafe {
                 UPlayFriendList friendList = new UPlayFriendList();
 
-                bool result = UPlayFriendsGetFriendList(0xFFu, &friendList);
+                bool result = UPlayFriendsGetFriendList(4, &friendList);
                 Debug.Assert(result);
 
                 if (friendList.friends != null) {
@@ -82,57 +83,16 @@ namespace S4_UIEngine.UPlay {
 
         public static void InviteFriend(Friend friend) {
             unsafe {
-                var inviteParams = new InviteParameters() { unknownProbablySize = 15 };
-                UPlayFriendsInviteToGame(friend.AccountId, &inviteParams);
+                var inviteParams = new InviteParameters() { unknownProbablySize = 15 }; //don't ask why
+                UPlayFriendsInviteToGame(Marshal.StringToHGlobalAnsi(friend.AccountId), &inviteParams);
             }
         }
-
-        /*
-         #pragma pack(push, 1)
-struct UplayFriend {
-		char* accountId;
-		char* name;
-		int relationship;
-		int avatarId;
-		DWORD gameSession;
-		char blacklisted;
-		BYTE unknown[3];
-		char* nickname;
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct UplayFriendList {
-	int length;
-	UplayFriend **friends;
-};
-
-#pragma pack(pop)
-typedef bool(__cdecl* _UPLAY_FRIENDS_GetFriendList)( int, UplayFriendList *);
-_UPLAY_FRIENDS_GetFriendList UPLAY_FRIENDS_GetFriendListManaged;
-
-#pragma pack(push, 1)
-struct InviteParameters {
-	void* __vftable /*VFT;
-    BYTE unknown[4];
-    DWORD unknown2;
-    BYTE unknownLarge[12];
-    DWORD unknown3;
-    BYTE unknownLarge2[12];
-    int unknown4;
-    DWORD unknownProbablySize;
-};
-#pragma pack(pop)
-
-typedef bool (__cdecl* _UPLAY_FRIENDS_InviteToGame) (char*, InviteParameters*);
-_UPLAY_FRIENDS_InviteToGame UPLAY_FRIENDS_InviteToGameManaged;
-         */
     }
 
     public struct Friend {
         public string PlayerName { get; }
         public string AccountId { get; }
-        public bool IsOnline { get; }
+        public bool IsOnline { get; } //Not Yet Implemented - Does not exists in the UPLAY_FRIENDS API - maybe with a different API?
 
         public Friend(string playerName, string accountId, bool isOnline) {
             this.PlayerName = playerName;
