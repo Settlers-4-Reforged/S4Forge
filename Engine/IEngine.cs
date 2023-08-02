616af48bcfaf5a84cb376247ea469666ac27b3ec
WizzardMaker <wizzardmaker@gmail.com>
2023-08-02 03:31:54 +0200

    added engine loader and interface

---
+++ Engine/IEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forge.Engine {
+    public interface IEngine {
+        string Name { get; }
+
+        bool Initialize(S4Forge forge);
+    }
+}
---
+++ Engine/ModuleLoader.cs
@@ -0,0 +1,45 @@
+﻿using NetModAPI;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Forge.Engine {
+    internal static class ModuleLoader {
+        public static Type? GetIEngine(Assembly assembly) {
+            Type[] types = assembly.GetExportedTypes();
+
+            Type? iEngine = (from Type t in types
+                             where t.GetInterface(nameof(IEngine)) != null
+                             select t).FirstOrDefault();
+
+            return iEngine;
+        }
+
+        private static readonly string[] Engines = new string[] { "UX-Engine.dll", "Plugin-Engine.dll" };
+        private const string EnginePath = "\\plugins\\";
+
+        public static IEnumerable<IEngine> CreateAvailableEngines() {
+            List<IEngine> engines = new List<IEngine>();
+
+            foreach (string engine in Engines) {
+                try {
+                    Assembly engineAssembly = Assembly.LoadFile(Environment.CurrentDirectory + EnginePath + engine);
+                    Type? iEngine = GetIEngine(engineAssembly);
+                    if (iEngine == null) {
+                        throw new EntryPointNotFoundException($"Failed to find IEngine in engine \"{engine}\"");
+                    }
+
+                    IEngine engineObject = (IEngine)Activator.CreateInstance(iEngine);
+                    engines.Add(engineObject);
+                } catch (Exception ex) {
+                    Logger.LogError($"Failed to load engine \"{engine}\" at \"{Environment.CurrentDirectory + EnginePath}\"", ex);
+                }
+            }
+
+            return engines;
+        }
+    }
+}
