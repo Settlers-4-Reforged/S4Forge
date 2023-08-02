2b474365a5b70fff8afedb6b2db0698cfb7b37ff
WizzardMaker <wizzardmaker@gmail.com>
2023-08-02 03:31:28 +0200

    added ModAPI wrapper thunk

---
+++ Native/ModAPI.cs
@@ -0,0 +1,15 @@
+﻿using NetModAPI;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forge.Native {
+    public static class ModAPI {
+        public static NetS4ModApi API;
+
+        static ModAPI() {
+            API = new NetS4ModApi();
+        }
+    }
+}
