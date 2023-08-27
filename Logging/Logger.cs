46ae0b23d787333d2a4848fe702585c2b05952e4
WizzardMaker <wizzardmaker@gmail.com>
2023-08-28 00:30:47 +0200

    added logger wrapper

---
+++ Logging/Logger.cs
@@ -0,0 +1,58 @@
+﻿using Forge.Engine;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using NetLogger = NetModAPI.Logger;
+
+namespace Forge.Logging {
+    public static class Logger {
+        public static bool Enabled = true;
+
+        private static string GetEngineSource(Assembly caller) {
+            var engine = ModuleLoader.GetIEngine(caller);
+            if (engine == null)
+                return "Generic";
+
+
+            return engine.Name;
+        }
+
+        public static void LogInfo(string message, params object[] args) {
+            if (!Enabled)
+                return;
+
+            string source = GetEngineSource(Assembly.GetCallingAssembly());
+            NetLogger.LogInfo($"[{source}] " + string.Format(message, args));
+        }
+        public static void LogWarn(string message, params object[] args) {
+            if (!Enabled)
+                return;
+
+            string source = GetEngineSource(Assembly.GetCallingAssembly());
+            NetLogger.LogWarn($"[{source}] " + string.Format(message, args));
+        }
+
+        public static void LogDebug(string message, params object[] args) {
+            if (!Enabled)
+                return;
+
+            string source = GetEngineSource(Assembly.GetCallingAssembly());
+            NetLogger.LogDebug($"[{source}] " + string.Format(message, args));
+        }
+
+        public static void LogError(Exception? err, string? message, params object[] args) {
+            if (!Enabled)
+                return;
+
+            string source = GetEngineSource(Assembly.GetCallingAssembly());
+            NetLogger.LogError($"[{source}] " + string.Format(message, args), err);
+        }
+
+    }
+}
+
