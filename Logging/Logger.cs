eda322c0ca03241c6abca8e94f66b6962403b944
WizzardMaker <wizzardmaker@gmail.com>
2023-09-02 13:38:05 +0200

    updated logging format

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -14,12 +14,8 @@ namespace Forge.Logging {
         public static bool Enabled = true;
 
         private static string GetEngineSource(Assembly caller) {
-            var engine = ModuleLoader.GetIEngine(caller);
-            if (engine == null)
-                return "Generic";
-
-
-            return engine.Name;
+            Type? engine = ModuleLoader.GetIEngine(caller);
+            return engine == null ? "Generic" : engine.Name;
         }
 
         public static void LogInfo(string message, params object[] args) {
@@ -27,14 +23,14 @@ namespace Forge.Logging {
                 return;
 
             string source = GetEngineSource(Assembly.GetCallingAssembly());
-            NetLogger.LogInfo($"[{source}] " + string.Format(message, args));
+            NetLogger.LogInfo($"[{source}]>>" + string.Format(message, args));
         }
         public static void LogWarn(string message, params object[] args) {
             if (!Enabled)
                 return;
 
             string source = GetEngineSource(Assembly.GetCallingAssembly());
-            NetLogger.LogWarn($"[{source}] " + string.Format(message, args));
+            NetLogger.LogWarn($"[{source}]>>" + string.Format(message, args));
         }
 
         public static void LogDebug(string message, params object[] args) {
@@ -42,7 +38,7 @@ namespace Forge.Logging {
                 return;
 
             string source = GetEngineSource(Assembly.GetCallingAssembly());
-            NetLogger.LogDebug($"[{source}] " + string.Format(message, args));
+            NetLogger.LogDebug($"[{source}]>>" + string.Format(message, args));
         }
 
         public static void LogError(Exception? err, string? message, params object[] args) {
@@ -50,7 +46,7 @@ namespace Forge.Logging {
                 return;
 
             string source = GetEngineSource(Assembly.GetCallingAssembly());
-            NetLogger.LogError($"[{source}] " + string.Format(message, args), err);
+            NetLogger.LogError($"[{source}]>>" + string.Format(message, args), err);
         }
 
     }
