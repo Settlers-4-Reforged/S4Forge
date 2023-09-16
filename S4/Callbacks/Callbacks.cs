dc35826f77565dc9ba6be5031d63f90e5a746b8f
WizzardMaker <wizzardmaker@gmail.com>
2023-09-17 00:48:20 +0200

    added more information to Callbacks
    also renamed callbacks to better define intent

--- S4/Callbacks/Callbacks.cs
+++ S4/Callbacks/Callbacks.cs
@@ -1,4 +1,5 @@
 ﻿using Forge.Native;
+using Forge.S4.Types;
 
 using Microsoft.DirectX.DirectDraw;
 
@@ -6,43 +7,33 @@ using NetModAPI;
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using DirectDrawSurface = Microsoft.DirectX.DirectDraw.Surface;
 
 namespace Forge.S4.Callbacks {
     public static class Callbacks {
-        /*
-        S4FrameCallback(LPDIRECTDRAWSURFACE7 lpSurface, INT32 iPillarboxWidth, LPVOID lpReserved)
-        S4MapInitCallback(LPVOID lpReserved0, LPVOID lpReserved1)
-        S4MouseCallback(DWORD dwMouseButton, INT iX, INT iY, DWORD dwMsgId, HWND hwnd, LPCS4UIELEMENT lpUiElement)
-        S4SettlerSendCallback(DWORD dwPosition, S4_MOVEMENT_ENUM dwCommand, LPVOID lpReserved)
-        S4TickCallback(DWORD dwTick, BOOL bHasEvent, BOOL bIsDelayed)
-        S4LuaOpenCallback()
-        S4BltCallback(LPS4BLTPARAMS params, BOOL discard)
-        S4GuiBltCallback(LPS4GUIBLTPARAMS params, BOOL discard)
-        S4EntityCallback(WORD entity, S4_ENTITY_CAUSE cause); // called when an entity is spawned or destructed
-        S4GuiDrawCallback(LPS4GUIDRAWBLTPARAMS entity, BOOL discard)
-        S4GuiClearCallback(LPS4GUICLEARPARAMS entity, BOOL discard)
-        */
+
+        #region Delegates
 
         public delegate void GameFrameCallback(DirectDrawSurface? surface, int pillarBoxWidth);
         public delegate void MapInitCallback();
-        public delegate void MouseCallback(uint mouseButton, int x, int y, uint msgId, IntPtr hwnd, IntPtr uiElement);
-        public delegate void SettlerSendCallback(uint position, uint command, IntPtr reserved);
+        public unsafe delegate void MouseCallbackUnsafe(uint mouseButton, int x, int y, uint msgId, S4UIElement* uiElement);
+        public delegate void MouseCallback(uint mouseButton, int x, int y, uint msgId, S4UIElement? uiElement);
+        public delegate void SettlerSendCallback(uint position, uint command);
         public delegate void TickCallback(uint tick, bool hasEvent, bool isDelayed);
         public delegate void LuaOpenCallback();
-        public delegate void BltCallback(IntPtr bltParams, bool discard);
-        public delegate void GuiBltCallback(IntPtr bltParams, bool discard);
+        public delegate void DrawEntityCallback(S4EntityDrawParams parameter, bool discard);
+        public delegate void SurfaceDrawCallback(IntPtr bltParams, bool discard);
         public delegate void EntityCallback(ushort entity, ushort cause);
-        public delegate void GuiDrawCallback(IntPtr bltParams, bool discard);
-        public delegate void GuiClearCallback(IntPtr bltParams, bool discard);
+        public delegate void UIElementDrawCallback(S4UIElementDrawParams bltParams, S4UIElement? uiElement, bool discard);
+        public unsafe delegate void UIElementDrawCallbackUnsafe(S4UIElementDrawParams bltParams, S4UIElement* uiElement, bool discard);
+        public delegate void SurfaceClearCallback(IntPtr bltParams, bool discard);
 
+        #endregion
 
-        /// <summary>
-        /// Event that is called every frame.
-        /// </summary>
-        public static event GameFrameCallback? OnFrame;
+        #region Initializations
 
         /// <summary>
         /// Event that is called when the map is initialized.
@@ -50,9 +41,27 @@ namespace Forge.S4.Callbacks {
         public static event MapInitCallback? OnMapInit;
 
         /// <summary>
+        /// Event that is called when a map script is opened.
+        /// </summary>
+        public static event LuaOpenCallback? OnLuaOpen;
+
+        #endregion
+
+        #region Interaction
+
+        /// <summary>
         /// Event that is called when the mouse is clicked.
         /// </summary>
         public static event MouseCallback? OnMouse;
+        /// <summary>
+        /// Event that is called when the mouse is clicked.
+        /// UI element pointer to game memory is passed.
+        /// </summary>
+        public static event MouseCallbackUnsafe? OnMouseUnsafe;
+
+        #endregion
+
+        #region InGameEvents
 
         /// <summary>
         /// Event that is called when a settler move command is sent.
@@ -65,39 +74,49 @@ namespace Forge.S4.Callbacks {
         public static event TickCallback? OnTick;
 
         /// <summary>
-        /// Event that is called when a map script is opened.
-        /// </summary>
-        public static event LuaOpenCallback? OnLuaOpen;
-
-        /// <summary>
-        /// Event that is called when a blt is called.
-        /// </summary>
-        public static event BltCallback? OnBlt;
-
-        /// <summary>
-        /// Event that is called when a gui surface is drawn onto the screen.
-        /// </summary>
-        public static event GuiBltCallback? OnGuiBlt;
-
-        /// <summary>
         /// Event that is called when an entity is spawned or destructed.
         /// </summary>
         public static event EntityCallback? OnEntity;
 
+        #endregion
+
+        #region Rendering
+        /// <summary>
+        /// Event that is called every frame.
+        /// </summary>
+        public static event GameFrameCallback? OnFrame;
+
+        /// <summary>
+        /// Event that is called when any "in-world" element is drawn.
+        /// </summary>
+        public static event DrawEntityCallback? OnDrawEntity;
+
+        /// <summary>
+        /// Event that is called when a gui surface is drawn onto the screen.
+        /// </summary>
+        public static event SurfaceDrawCallback? OnSurfaceDraw;
+
         /// <summary>
         /// Event that is called when a gui element is drawn onto a gui surface.
         /// </summary>
-        public static event GuiDrawCallback? OnGuiDraw;
+        public static event UIElementDrawCallback? OnUIElementDraw;
+
+        /// <summary>
+        /// Event that is called when a gui element is drawn onto a gui surface.
+        /// UI element pointer to game memory is passed.
+        /// </summary>
+        public static event UIElementDrawCallbackUnsafe? OnUIElementDrawUnsafe;
 
         /// <summary>
         /// Event that is called when a gui surface is cleared.
         /// </summary>
-        public static event GuiClearCallback? OnGuiClear;
+        public static event SurfaceClearCallback? OnSurfaceClear;
 
+        #endregion
 
 
         static unsafe Callbacks() {
-            ModAPI.API.AddFrameListener((IDirectDrawSurface7* surface, Int32 width, void* reserved) => {
+            ModAPI.API.AddFrameListener((surface, width, reserved) => {
                 using Surface? directDrawSurface = DDrawUtil.SurfaceFromPointer(surface);
 
                 OnFrame?.Invoke(directDrawSurface, width);
@@ -106,26 +125,28 @@ namespace Forge.S4.Callbacks {
                 return 0;
             });
 
-            ModAPI.API.AddMapInitListener((void* reserved0, void* reserved1) => {
+            ModAPI.API.AddMapInitListener((reserved0, reserved1) => {
                 OnMapInit?.Invoke();
                 //once reserved0 or reserved1 is no longer unused, add new version of OnMapInit
 
                 return 0;
             });
 
-            ModAPI.API.AddMouseListener((uint mouseButton, int x, int y, uint msgId, HWND__* hwnd, S4UiElement* uiElement) => {
-                OnMouse?.Invoke(mouseButton, x, y, msgId, new IntPtr(hwnd), new IntPtr(uiElement));
+            ModAPI.API.AddMouseListener((mouseButton, x, y, msgId, hwnd, uiElement) => {
+                OnMouse?.Invoke(mouseButton, x, y, msgId, uiElement == null ? null : Marshal.PtrToStructure<S4UIElement>(new IntPtr(uiElement)));
+                OnMouseUnsafe?.Invoke(mouseButton, x, y, msgId, (S4UIElement*)uiElement);
 
                 return 0;
             });
 
-            ModAPI.API.AddSettlerSendListener((uint position, S4_MOVEMENT_ENUM command, void* reserved) => {
-                OnSettlerSend?.Invoke(position, (uint)command, new IntPtr(reserved));
+            ModAPI.API.AddSettlerSendListener((position, command, reserved) => {
+                OnSettlerSend?.Invoke(position, (uint)command);
+                //once reserved is no longer unused, add new version of OnSettlerSend
 
                 return 0;
             });
 
-            ModAPI.API.AddTickListener((uint tick, int hasEvent, int isDelayed) => {
+            ModAPI.API.AddTickListener((tick, hasEvent, isDelayed) => {
                 OnTick?.Invoke(tick, hasEvent != 0, isDelayed != 0);
 
                 return 0;
@@ -137,32 +158,37 @@ namespace Forge.S4.Callbacks {
                 return 0;
             });
 
-            ModAPI.API.AddBltListener((S4BltParams* bltParams, int discard) => {
-                OnBlt?.Invoke(new IntPtr(bltParams), discard != 0);
+            ModAPI.API.AddBltListener((bltParams, discard) => {
+                OnDrawEntity?.Invoke(Marshal.PtrToStructure<S4EntityDrawParams>(new IntPtr(bltParams)), discard != 0);
 
                 return 0;
             });
 
-            ModAPI.API.AddGuiBltListener((S4GuiBltParams* bltParams, int discard) => {
-                OnGuiBlt?.Invoke(new IntPtr(bltParams), discard != 0);
+            ModAPI.API.AddGuiBltListener((bltParams, discard) => {
+                OnSurfaceDraw?.Invoke(new IntPtr(bltParams), discard != 0);
 
                 return 0;
             });
 
-            ModAPI.API.AddEntityListener((ushort entity, S4_ENTITY_CAUSE cause) => {
+            ModAPI.API.AddEntityListener((entity, cause) => {
                 OnEntity?.Invoke(entity, (ushort)cause);
 
                 return 0;
             });
 
-            ModAPI.API.AddGuiElementBltListener((S4GuiElementBltParams* bltParams, int discard) => {
-                OnGuiDraw?.Invoke(new IntPtr(bltParams), discard != 0);
+            ModAPI.API.AddGuiElementBltListener((bltParams, discard) => {
+                var param = Marshal.PtrToStructure<S4UIElementDrawParams>(new IntPtr(bltParams));
+
+                S4UIElement* uiElement = GameValues.GetUIElementFromIndexUnsafe(param.elementContainerId, param.valueLink);
+
+                OnUIElementDraw?.Invoke(param, uiElement == null ? null : *uiElement, discard != 0);
+                OnUIElementDrawUnsafe?.Invoke(param, uiElement, discard != 0);
 
                 return 0;
             });
 
-            ModAPI.API.AddGuiClearListener((S4GuiClearParams* bltParams, int discard) => {
-                OnGuiClear?.Invoke(new IntPtr(bltParams), discard != 0);
+            ModAPI.API.AddGuiClearListener((bltParams, discard) => {
+                OnSurfaceClear?.Invoke(new IntPtr(bltParams), discard != 0);
 
                 return 0;
             });
---
+++ S4/Callbacks/Types.cs
@@ -0,0 +1,65 @@
+﻿using Forge.S4.Types;
+
+using Microsoft.DirectX.Direct3D;
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Forge.S4.Callbacks {
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct S4EntityDrawParams {
+        public IntPtr caller;
+        public IntPtr /*<word>*/ imagePalette;
+        public IntPtr /*<byte>*/ imageData;
+        public Int32 imageWidth;
+        public Int32 imageHeight;
+        public Int32 destX;
+        public Int32 destY;
+        public Int32 destClippingOffsetY;
+        public IntPtr /*<word>*/ subSurface;
+        public bool imageHighRes;
+        public Int32 destWidth;
+        public Int32 destHeight;
+        public Int32 surfaceWidth;
+        public Int32 surfaceHeight;
+        public Int32 stride;
+        public Int32 zoomFactor;
+        public IntPtr /*<word>*/ surface;
+        public bool isFogOfWar;
+        public Int16 settlerId;
+        public Int16 spriteId;
+        public IntPtr /*<HDC>*/ destinationDc;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct S4UIElementDrawParams {
+        public Int32 surfaceWidth;
+        public Int32 surfaceHeight;
+        public Int16 activeGFXCollection;
+        public Int16 elementContainerId;
+        public Int16 x;
+        public Int16 y;
+        public Int16 xOffset;
+        public Int16 yOffset;
+        public Int16 width;
+        public Int16 height;
+        public Int16 mainTexture;
+        public Int16 valueLink;
+        public Int16 buttonPressedTexture;
+        public Int16 tooltipLink;
+        public Int16 tooltipLinkExtra;
+        public UIElementTypes elementType;
+        public UIElementEffects effects; //When == 8 -> hide text
+        //public enum where the first 4 bits define which font style to use and last 4 bits define effects (Like pressed etc)
+        public UIElementTextStyle textStyle;
+        public Int16 showTexture;
+        public Int16 backTexture;
+        public IntPtr text;
+        public IntPtr tooltipText;
+        public IntPtr tooltipExtraText;
+    }
+}
