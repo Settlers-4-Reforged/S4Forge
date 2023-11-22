f6bce51e4a5b364afb1e0242bedba296501718a9
WizzardMaker <wizzardmaker@gmail.com>
2023-11-22 14:02:16 +0100

    added custom windows wndproc

--- Native/User32.cs
+++ Native/User32.cs
@@ -5,11 +5,43 @@ using System.Text;
 
 namespace Forge.Native {
     public class User32 {
+        public struct Pos {
+            public long X;
+            public long Y;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetCursorPos(out Pos lpPoint);
+
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
 
         public static void MessageBox(string text, string caption) {
             MessageBox(IntPtr.Zero, text, caption, 0);
         }
+
+        internal static void RegisterWndProc() {
+            unsafe {
+                ModAPI.API.AddWndProc(((_, msg, param, lParam) => {
+
+                    var winMains = WndProc?.GetInvocationList();
+
+                    if (winMains == null) return 0;
+                    foreach (Delegate? winMain in winMains) {
+                        if (winMain is not Func<WndProcMsg, IntPtr, IntPtr, bool> func) continue;
+
+                        if (func((WndProcMsg)msg, new IntPtr(param), new IntPtr(lParam))) {
+                            return 0;
+                        }
+                    }
+
+                    return 0;
+                }));
+            }
+
+
+        }
+
+        public static Func<WndProcMsg, IntPtr, IntPtr, bool>? WndProc { get; set; }
     }
 }
