d527c860a5c517d9cd8f33e0177844594072783b
WizzardMaker <wizzardmaker@gmail.com>
2024-01-27 18:08:12 +0100

    added private logger for individual logs

--- Logging/Logger.cs
+++ Logging/Logger.cs
@@ -10,12 +10,40 @@ using System.Text;
 using NetLogger = NetModAPI.Logger;
 
 namespace Forge.Logging {
-    public static class Logger {
+    public abstract class Logger {
         public static bool Enabled = true;
+
+        public class PrivateLogger : Logger {
+            private readonly string name;
+            public PrivateLogger(string name) {
+                NetLogger.AddLogger(name);
+                this.name = name;
+            }
+
+            public new void LogDebug(string message, params object[] args) {
+                NetLogger.LogDebug(string.Format(message, args), name);
+            }
+
+            public new void LogError(Exception? err, string message, params object[] args) {
+                NetLogger.LogError(string.Format(message, args), err, name);
+            }
+
+            public new void LogInfo(string message, params object[] args) {
+                NetLogger.LogInfo(string.Format(message, args), name);
+            }
+
+            public new void LogWarn(string message, params object[] args) {
+                NetLogger.LogWarn(string.Format(message, args), name);
+            }
+        }
 
         private static string GetEngineSource(Assembly caller) {
             Type? engine = ModuleLoader.GetIEngine(caller);
             return engine == null ? "Generic" : engine.Name;
+        }
+
+        public static PrivateLogger CreateLogger(string name) {
+            return new PrivateLogger(name);
         }
 
         public static void LogInfo(string message, params object[] args) {
@@ -48,7 +76,6 @@ namespace Forge.Logging {
             string source = GetEngineSource(Assembly.GetCallingAssembly());
             NetLogger.LogError($"[{source}]>>" + string.Format(message, args), err);
         }
-
     }
 }
 
