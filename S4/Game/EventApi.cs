b25920ace3906954f5db89408fb6697c4c1a64c2
WizzardMaker <wizzardmaker@gmail.com>
2024-03-01 17:47:08 +0100

    added s4 event api

---
+++ S4/Game/EventApi.cs
@@ -0,0 +1,28 @@
+﻿using Forge.Native;
+using Forge.S4.Types;
+using Forge.S4.Types.Native;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.S4.Game {
+
+    public interface IEventApi {
+        void SendEvent(EventType type, uint wparam, uint lparam, sbyte unknown);
+    }
+
+    public class EventApi : IEventApi {
+        private unsafe void* EventManager => (void*)GameValues.ReadValue<int>(0x106B11C);
+
+        public void SendEvent(EventType type, uint wparam, uint lparam, sbyte unknown) {
+            unsafe {
+                Event newEvent;
+                void* s4event = ModAPI.API.CreateS4Event(&newEvent, (uint)type, wparam, lparam, unknown);
+                ModAPI.API.PostToMessageQueue(EventManager, s4event);
+            }
+        }
+    }
+}
---
+++ S4/Types/Native/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.S4.Types.Native {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public unsafe struct Event {
+        void* __vftable /*VFT*/;
+        EventType eventType;
+        uint wparam;
+        uint lparam;
+        uint eventEngineAttribute;
+        byte unknown;
+    }
+}
