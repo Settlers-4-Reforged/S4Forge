a8bcd443f36c0a55773c7bea4f2e4d39d3730b88
WizzardMaker <wizzardmaker@gmail.com>
2024-03-01 20:57:17 +0100

    added sound api

---
+++ S4/Game/SoundApi.cs
@@ -0,0 +1,46 @@
+﻿using Forge.Native;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.S4.Game {
+    public interface ISoundApi {
+        void PlaySound(int id);
+
+        int MusicVolume { get; set; }
+        int SoundVolume { get; set; }
+    }
+
+    public class SoundApi : ISoundApi {
+        public void PlaySound(int id) {
+            ModAPI.API.PlaySound(id);
+        }
+
+        public int MusicVolume {
+            get => ModAPI.API.GetMusicEnabled() == 0 ? 0 : ModAPI.API.GetMusicVolume();
+            set {
+                if (value == 0) {
+                    ModAPI.API.ToggleMusic(false);
+                } else {
+                    ModAPI.API.ToggleMusic(false);
+                    ModAPI.API.SetMusicVolume(value);
+                }
+            }
+        }
+
+        public int SoundVolume {
+            get => ModAPI.API.GetSoundEnabled() == 0 ? 0 : ModAPI.API.GetSoundVolume();
+            set {
+                if (value == 0) {
+                    ModAPI.API.ToggleSound(false);
+                } else {
+                    ModAPI.API.ToggleSound(false);
+                    ModAPI.API.SetSoundVolume(value);
+                }
+            }
+        }
+    }
+}
--- S4/Managers/ApiManager.cs
+++ S4/Managers/ApiManager.cs
@@ -9,6 +9,7 @@ using System.Text;
 namespace Forge.S4.Managers {
     internal class ApiManager {
         public static void RegisterDependencies() {
+            DI.Dependencies.Register<Game.ISoundApi, Game.SoundApi>(Reuse.Singleton);
             DI.Dependencies.Register<Game.IEventApi, Game.EventApi>(Reuse.Singleton);
             DI.Dependencies.Register<Game.PlayerApi, Game.PlayerApi>(Reuse.Singleton);
         }
