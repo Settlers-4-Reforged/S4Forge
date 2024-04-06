9a2b4d760e0f4516adcf1d5c5b45429804e554ac
WizzardMaker <wizzardmaker@gmail.com>
2024-04-06 21:36:46 +0200

    added more attributes to Player class

--- S4/Types/Player.cs
+++ S4/Types/Player.cs
@@ -4,13 +4,22 @@ using System.Text;
 
 namespace Forge.S4.Types {
     public class Player {
-        public uint Id;
+        const int PlayerCount = 8;
+        private static readonly Player?[] players = new Player[PlayerCount];
+        public static Player FromId(uint id) => players[id] ??= new Player(id);
 
-        internal Player(uint id) {
-            Id = id;
-        }
+        private Player() { }
+        private Player(uint id) => Id = id;
+
+        public uint Id { get; init; }
 
         // TODO: replace with direct memory access
         public int OffensiveFightingStrength => (int)Forge.Native.ModAPI.API.GetOffenceFightingStrength(Id);
+
+        public Tribe Tribe => (Tribe)Forge.Native.ModAPI.API.GetPlayerTribe(Id);
+
+
+        public bool HasLost => Forge.Native.ModAPI.API.HasPlayerLost(Id) != 0;
+
     }
 }
