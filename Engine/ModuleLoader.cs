4dd3d1d82b2a24999bf7ec8bbb8e556a434dd5f3
WizzardMaker <wizzardmaker@gmail.com>
2024-05-24 19:42:00 +0200

    improved logging for module loader

--- Engine/ModuleLoader.cs
+++ Engine/ModuleLoader.cs
@@ -20,13 +20,22 @@ namespace Forge.Engine {
             return iEngine;
         }
 
-        private static readonly string[] Engines = new string[] { "UX-Engine.dll" };
+        private static readonly string[] Engines = new string[] { "UX-Engine.dll", "NetEngine.dll" };
         private const string EnginePath = @"\plugins\Forge\Engines\";
+        private static string AbsoluteEnginePath => Environment.CurrentDirectory + EnginePath;
 
         internal static void RegisterAvailableEngines(Container dependencies) {
+            Logger.LogInfo("Searching for available engines...");
+            Logger.LogDebug($"Searching for the following engines: {string.Join(", ", Engines)}");
+
             foreach (string engine in Engines) {
+                if (!System.IO.File.Exists(AbsoluteEnginePath + engine)) {
+                    Logger.LogWarn($"Engine \"{engine}\" not found at \"{AbsoluteEnginePath}\"");
+                    continue;
+                }
+
                 try {
-                    Assembly engineAssembly = Assembly.LoadFile(Environment.CurrentDirectory + EnginePath + engine);
+                    Assembly engineAssembly = Assembly.LoadFile(AbsoluteEnginePath + engine);
 
                     AssemblyInitializations.AddAssemblyLoadSource(engineAssembly);
 
@@ -36,8 +45,10 @@ namespace Forge.Engine {
                     }
 
                     dependencies.RegisterMany(new[] { iEngine }, Reuse.Singleton);
+
+                    Logger.LogInfo($"Registered engine \"{engine}\" at \"{AbsoluteEnginePath}\"");
                 } catch (Exception ex) {
-                    Logger.LogError($"Failed to load engine \"{engine}\" at \"{Environment.CurrentDirectory + EnginePath}\"", ex);
+                    Logger.LogError($"Failed to load engine \"{engine}\" at \"{AbsoluteEnginePath}\"", ex);
                 }
             }
 
