88a4709483198bd89595687e6aa5dad6092de2d9
WizzardMaker <wizzardmaker@gmail.com>
2024-05-27 20:03:08 +0200

    added GfxEngineApi placeholder

---
+++ S4/Game/GfxEngineApi.cs
@@ -0,0 +1,22 @@
+﻿using Forge.Config;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forge.S4.Game {
+
+    [GenerateAutomaticInterface]
+    internal class GfxEngineApi : IGfxEngineApi {
+        public GfxEngineApi() {
+
+        }
+
+        Vector2 ConvertMapPositionToWorldScreenOffsets(Vector2 mapPosition) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- S4/Managers/ApiManager.cs
+++ S4/Managers/ApiManager.cs
@@ -9,9 +9,10 @@ using System.Text;
 namespace Forge.S4.Managers {
     internal class ApiManager {
         public static void RegisterDependencies() {
-            DI.Dependencies.Register<Game.ISoundApi, Game.SoundApi>(Reuse.Singleton);
             DI.Dependencies.Register<Game.IEventApi, Game.EventApi>(Reuse.Singleton);
+            DI.Dependencies.Register<Game.IGfxEngineApi, Game.GfxEngineApi>(Reuse.Singleton);
             DI.Dependencies.Register<Game.IPlayerApi, Game.PlayerApi>(Reuse.Singleton);
+            DI.Dependencies.Register<Game.ISoundApi, Game.SoundApi>(Reuse.Singleton);
         }
     }
 }
