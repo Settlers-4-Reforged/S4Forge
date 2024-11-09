4653d7ab15272b6aeda2bce74c414e2705e0f015
WizzardMaker <wizzardmaker@gmail.com>
2024-11-09 16:15:22 +0100

    updated IPlugin interface

--- Engine/IPlugin.cs
+++ Engine/IPlugin.cs
@@ -1,14 +1,34 @@
-﻿using System;
+﻿using DryIoc;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Forge.Engine {
     public interface IPlugin {
         int Priority { get; }
-        string Version { get; }
-        string[] DependencyFolders { get; }
 
+        /// <summary>
+        /// Callback to register any further DI dependencies. This is called before Initialize and should not be used to implement any logic.
+        /// </summary>
+        /// <remarks>
+        /// This method is called in the order of the plugin priority
+        /// </remarks>
+        void RegisterDependencies(Container injector) { }
 
+        /// <summary>
+        /// Callback to initialize the plugin. This is called after RegisterDependencies and should be used to implement any plugin wide logic.
+        /// </summary>
+        /// <remarks>
+        /// This method is called in the order of the plugin priority
+        /// </remarks>
         void Initialize();
+    }
+
+    /// <summary>
+    /// A callback interface for DI registered classes that need to be called after all plugins have been loaded and initialized
+    /// </summary>
+    public interface IAfterPluginsLoaded {
+        void AfterPluginsLoaded();
     }
 }
--- Engine/PluginLoader.cs
+++ Engine/PluginLoader.cs
@@ -15,14 +15,17 @@ using System.Text.Json.Serialization;
 
 namespace Forge.Engine {
     internal static class PluginLoader {
+        /// <summary>
+        /// An extension to the ForgeUpdater manifest. Manifest struct is not imported to avoid dependency
+        /// </summary>
         class PluginInfo {
-            internal const string ConfigFileName = "plugin.cfg";
+            internal const string ConfigFileName = "manifest.json";
 
             /// <summary>
             /// The name of the plugin.
             /// </summary>
-            [JsonPropertyName("name")]
-            public required string Name { get; set; }
+            [JsonPropertyName("id")]
+            public required string Id { get; set; }
 
             /// <summary>
             /// The name of the .NET assembly file that contains the plugin implementations.
@@ -31,53 +34,36 @@ namespace Forge.Engine {
             public required string EntryPoint { get; set; }
 
             /// <summary>
-            /// The version of the plugin.
-            /// </summary>
-            /// <remarks>
-            /// Required format: `Major.Minor.Patch` (e.g. `1.0.0`)
-            /// </remarks>
-            [JsonPropertyName("version")]
-            public required string Version { get; set; }
-
-            /// <summary>
-            /// If the plugin is enabled or not.
-            /// </summary>
-            [JsonPropertyName("enabled")]
-            public required bool IsEnabled { get; set; }
-
-            /// <summary>
-            /// A additional folder with other dll dependencies to add to the library search path.
+            /// An additional folder with other dll dependencies to add to the library search path.
             /// </summary>
             [JsonPropertyName("library_folder")]
-            public string LibraryFolder { get; set; }
+            public string? LibraryFolder { get; set; }
 
             public struct Dependency {
                 /// <summary>
                 /// The name of the dependency according to it's plugin name.
                 /// </summary>
-                [JsonPropertyName("name")]
-                public required string Name { get; set; }
-
-                /// <summary>
-                /// The desired version of the dependency.
-                ///
-                /// This string can have wildcards, like `1.0.*` or `1.*` to match any such version inside the wildcard range.
-                /// </summary>
-                [JsonPropertyName("version")]
-                public required string Version { get; set; }
+                [JsonPropertyName("id")]
+                public required string Id { get; set; }
             }
 
             /// <summary>
             /// A list of dependencies that this plugin requires to function
             /// </summary>
-            [JsonPropertyName("dependencies")]
-            public Dependency[] Dependencies { get; set; } = { };
+            [JsonPropertyName("relationships")]
+            public Dependency[] Dependencies { get; set; } = Array.Empty<Dependency>();
 
-            public string directory;
+            public string? directory;
         }
 
         static readonly string PluginDirectory = Path.Combine(Environment.CurrentDirectory + "/plugins/Forge/Plugins");
         const string PluginLibFolder = "lib";
+
+        public static void InformPluginsLoadedCallbacks(Container dependencies) {
+            foreach (IAfterPluginsLoaded callback in dependencies.ResolveMany<IAfterPluginsLoaded>()) {
+                callback.AfterPluginsLoaded();
+            }
+        }
 
         public static bool LoadAllPlugins(Container dependencies) {
             string[] directories = Directory.GetDirectories(PluginDirectory);
@@ -112,27 +98,23 @@ namespace Forge.Engine {
 
             foreach (PluginInfo plugin in plugins) {
                 try {
+                    if (plugin.directory == null) {
+                        Logger.LogError(null, $"Plugin '{plugin.Id}' is missing a directory.");
+                        continue;
+                    }
+
                     string directory = plugin.directory;
 
                     bool missingDependencies = false;
-                    foreach (PluginInfo.Dependency dependency in plugin.Dependencies) {
-                        PluginInfo? foundDependency = plugins.FirstOrDefault(p => p.Name == dependency.Name);
-                        if (foundDependency == null) {
-                            Logger.LogError(null, $"Plugin '{directory}' has a dependency '{dependency.Name}' that does not exist.");
-                            missingDependencies = true;
-                            break;
-                        }
+                    string[] skippedDependencies = ["Forge", "UX-Engine"];
+                    var filteredDependencies = plugin.Dependencies.Where(d => !skippedDependencies.Contains(d.Id)).ToArray();
+                    foreach (PluginInfo.Dependency dependency in filteredDependencies) {
+                        PluginInfo? foundDependency = plugins.FirstOrDefault(p => p.Id == dependency.Id);
+                        if (foundDependency != null) continue;
 
-                        string dependencyVersion = dependency.Version;
-                        string foundDependencyVersion = foundDependency.Version;
-
-                        // Check if dependency version is in wildcard range
-                        if (!CompareVersionsWithWildcard(dependencyVersion, foundDependencyVersion)) {
-                            Logger.LogError(null, $"Plugin '{directory}' has a dependency '{dependency.Name}' with an incompatible version.");
-                            missingDependencies = true;
-                            break;
-                        }
-
+                        Logger.LogError(null, $"Plugin '{directory}' has a dependency '{dependency.Id}' that does not exist.");
+                        missingDependencies = true;
+                        break;
                     }
 
                     if (missingDependencies) {
@@ -143,11 +125,11 @@ namespace Forge.Engine {
                     // Check if plugin assembly exists
                     string pluginPath = Path.Combine(directory, plugin.EntryPoint);
                     if (!File.Exists(pluginPath)) {
-                        Logger.LogWarn("Plugin {0} is missing it's entry point assembly: '{1}'.", plugin.Name, pluginPath);
+                        Logger.LogWarn("Plugin {0} is missing it's entry point assembly: '{1}'.", plugin.Id, pluginPath);
                         continue;
                     }
 
-                    Logger.LogInfo($"Loading Plugin '{plugin.Name}'...");
+                    Logger.LogInfo($"Loading Plugin '{plugin.Id}'...");
 
                     AssemblyInitializations.AddFolderLoadSource(directory);
                     AssemblyInitializations.AddFolderLoadSource(Path.Combine(directory, PluginLibFolder));
@@ -159,7 +141,7 @@ namespace Forge.Engine {
                         e = e.InnerException;
                     }
 
-                    Logger.LogError(e, $"Error during load of plugin '{plugin.Name}'");
+                    Logger.LogError(e, $"Error during load of plugin '{plugin.Id}'");
                 }
             }
 
@@ -224,12 +206,27 @@ namespace Forge.Engine {
 
             foreach (IPlugin plugin in plugins) {
                 try {
-                    string msg =
-                        $"Initializing Plugin '{plugin.GetType().Name}' with a priority of {plugin.Priority}...";
-                    Logger.LogInfo(msg);
+                    Logger.LogInfo($"Registering Plugin dependencies for: '{plugin.GetType().Name}'...");
+                    plugin.RegisterDependencies(dependencies);
+                } catch (Exception e) {
+                    string stackTrace = e.StackTrace ?? "N/A";
+                    while (e.InnerException != null) {
+                        e = e.InnerException;
+                    }
+
+                    string errorMsg =
+                        $"Error during registration of Plugin dependencies for '{plugin.GetType().Name}'\nError: {e.Message}\n\n============= Stack Trace =============\n{stackTrace}";
+                    Logger.LogError(e, errorMsg);
+                }
+
+            }
+
+            foreach (IPlugin plugin in plugins) {
+                try {
+                    Logger.LogInfo($"Initializing Plugin '{plugin.GetType().Name}' with a priority of {plugin.Priority}...");
                     plugin.Initialize();
                 } catch (Exception e) {
-                    string stackTrace = e.StackTrace;
+                    string stackTrace = e.StackTrace ?? "N/A";
                     while (e.InnerException != null) {
                         e = e.InnerException;
                     }
